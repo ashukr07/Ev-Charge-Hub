50321fd15d55c69e2a3f10e511989bccd6afcf38
ashukr07 <ashutoshroy3434@gmail.com>
2025-04-19 20:10:01 +0530

    commands written for production

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './db/connectDB.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 
 import authRoutes from "./routes/auth.route.js";
 import stationRoutes from "./routes/station.route.js";
@@ -15,6 +15,8 @@ import paymentRoutes from "./routes/payment.route.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
 
 app.use(cors({
   origin: "http://localhost:5173", // ✅ Allow frontend to make requests
@@ -33,6 +35,13 @@ app.use("/api/bookings", bookingRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 app.use("/api/payment", paymentRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 
 app.listen(PORT, () => {

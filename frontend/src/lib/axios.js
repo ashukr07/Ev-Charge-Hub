c07fcda6ed87dbaa6d15605901c96ff1c6eb93b7
ashukr07 <ashutoshroy3434@gmail.com>
2025-04-19 20:24:45 +0530

    base url for prod

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //console.log(import.meta.mode)
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : '/api',
     withCredentials: true, //send cookies to the server
 })
 //console.log(axiosInstance.baseURL)
